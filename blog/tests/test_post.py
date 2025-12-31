ae5b5448e45b1ce8ff14bc66c74ed72ae8799bcc
wonbin <wonbin3977@gmail.com>
2025-12-31 11:01:12 +0900

    test(post) : category param 으로 왔을 때 처리 테스트 완료

--- blog/tests/test_post.py
+++ blog/tests/test_post.py
@@ -2,10 +2,12 @@ from django.test import TestCase
 from blog.models import Post,Category
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-class PostModelTest(TestCase):
+class PostModelTest(APITestCase):
 
     def setUp(self):
+        self.client = APIClient()
         self.category1 = Category.objects.create(name='testCategory1')
         self.category2 = Category.objects.create(name = 'testCategory2')
         
@@ -36,4 +38,10 @@ class PostModelTest(TestCase):
         titles = [post['title'] for post in response.data['results']]
         self.assertIn('First Post', titles)
         self.assertIn('Second Post', titles)
-        
+    
+    def test_post_list_with_category(self):
+        self.list_url = reverse('post-list')
+        response = self.client.get(self.list_url, {'category_id': self.category1.id})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['title'], 'First Post')
